0ccd729b714b67a1a623f58393fde047d0e74468
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2016-08-19 15:08:29 -0700

    data prep

--- 02_clean.R
+++ 02_clean.R
@@ -10,3 +10,59 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+##loading R libraries for script
+library(reshape2) #convert dataframe from wide to long format
+library(dplyr) #pipe function, data manipulation
+
+
+##making 'normalized' data set with gdpdata file for comparison of measures
+##adding ghg pop and gdp normalized columns
+gdpdata$ghgnorm <- gdpdata$BC_GHG_Emissions_ktCO2e/gdpdata$BC_GHG_Emissions_ktCO2e[gdpdata$Year == min(gdpdata$Year)]
+gdpdata$gdpnorm <- gdpdata$GDP/gdpdata$GDP[gdpdata$Year == min(gdpdata$Year)]
+gdpdata$popnorm <- gdpdata$Population/gdpdata$Population[gdpdata$Year == min(gdpdata$Year)]
+
+gdpdata.norm <- subset(gdpdata, select=c("Year", "ghgnorm", "gdpnorm", "popnorm"))
+
+
+##Creating separate ENERGY sector dataframe
+##subsetting rows
+ghgenergy <- subset(ghgdata, ghgdata$sector == "ENERGY")
+
+
+##creating 16 generalized subsubsector labels for facet plot
+ghgenergy$general_source <- ghgenergy$subsector_level2
+ghgenergy$general_source[ghgenergy$general_source == "Road Transportation"] <- "Road"
+ghgenergy$general_source[ghgenergy$general_source == "Railways"] <- "Railway"
+ghgenergy$general_source[ghgenergy$subsector_level3 == "Pipeline Transport"] <- "Pipeline Transport"
+ghgenergy$general_source[ghgenergy$subsector_level3 == "Off-Road Gasoline"] <- "Off-Road"
+ghgenergy$general_source[ghgenergy$subsector_level3 == "Off-Road Diesel"] <- "Off-Road"
+
+ghgenergy <- subset(ghgenergy, select=c("sector", "subsector_level1", "general_source",
+                                        "1990","1991","1992","1993","1994","1995",
+                                        "1996","1997","1998","1999","2000","2001",
+                                        "2002","2003","2004","2005","2006","2007",
+                                        "2008","2009","2010","2011", "2012", "2013", "2014"))
+
+##Editing some source names for plot label formatting
+ghgenergy$general_source <- gsub("and", "&", ghgenergy$general_source)
+
+
+##Converting wide files to long format 
+##using reshape to melt the data 
+ghgmelt <- reshape2::melt(ghgdata, id.vars=c("sector","subsector_level1","subsector_level2","subsector_level3"),
+                          variable.name="year", value.name="ktCO2e")
+
+ghgenergymelt <- reshape2::melt(ghgenergy, id.vars=c("sector","subsector_level1","general_source"),
+                                variable.name="Year", value.name="ktCO2e")
+
+gdpnormmelt <- reshape2::melt(gdpdata.norm, id.vars=c("Year"),
+                              variable.name="Indicator",
+                              value.name="Amount")
+
+
+##Changing year from factor to character manually
+ghgmelt$year <- as.integer(as.character(ghgmelt$year))
+ghgenergymelt$Year <- as.integer(as.character(ghgenergymelt$Year))
+gdpnormmelt$Year <- as.integer(as.character(gdpnormmelt$Year))
+
+
