3852b45112c308960059f3a84f5944339f373de4
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-08-10 16:30:53 -0700

    refactoring

--- 02_clean.R
+++ 02_clean.R
@@ -1,4 +1,4 @@
-# Copyright 2016 Province of British Columbia
+# Copyright 2018 Province of British Columbia
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,12 +10,34 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
-##loading R libraries for script
-library(reshape2) #convert dataframe from wide to long format
-library(dplyr) #pipe function, data manipulation
+library(tidyr) #wide df to long df
+library(dplyr) #data munging
+library(stringr)
 
 
-##making 'normalized' data set with gdpdata file for comparison of measures
+## Convert wide to long format 
+bc_ghg_long <- bc_ghg %>% 
+  gather(key =  year, value = ktCO2e,
+       -sector, -subsector_level1,
+       -subsector_level2, -subsector_level3) %>%
+  mutate(ktCO2e = as.numeric(ktCO2e),
+         year = as.integer(as.character(year)))
+
+
+## Create separate ENERGY dataframe
+bc_ghg_energy <- bc_ghg_long %>% 
+  filter(sector == "ENERGY") %>% 
+  mutate(general_source = case_when(subsector_level2 == "Road Transportation" ~ "Road",
+                                    subsector_level2 == "Railways" ~ "Railway",
+                                    subsector_level3 == "Pipeline Transport" ~ "Pipeline Transport",
+                                    subsector_level3 == "Off-Road Gasoline" ~ "Off-Road",
+                                    subsector_level3 == "Off-Road Diesel" ~ "Off-Road",
+                                    TRUE ~ subsector_level2),
+         general_source = str_replace(general_source, "and", "&")) %>% 
+  select(sector, subsector_level1, general_source, year, ktCO2e)
+
+
+## Make 'normalized' columns in bc_pop_gdp gdpdata file for comparison of measures
 ##adding ghg pop and gdp normalized columns
 gdpdata$ghgnorm <- gdpdata$BC_GHG_Emissions_ktCO2e/gdpdata$BC_GHG_Emissions_ktCO2e[gdpdata$Year == min(gdpdata$Year)]
 gdpdata$gdpnorm <- gdpdata$GDP/gdpdata$GDP[gdpdata$Year == min(gdpdata$Year)]
@@ -49,8 +71,7 @@ ghgenergy$general_source <- gsub("and", "&", ghgenergy$general_source)
 
 ##Converting wide files to long format 
 ##using reshape to melt the data 
-ghgmelt <- reshape2::melt(ghgdata, id.vars=c("sector","subsector_level1","subsector_level2","subsector_level3"),
-                          variable.name="year", value.name="ktCO2e")
+
 
 ghgenergymelt <- reshape2::melt(ghgenergy, id.vars=c("sector","subsector_level1","general_source"),
                                 variable.name="Year", value.name="ktCO2e")
