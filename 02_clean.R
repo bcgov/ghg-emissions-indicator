14544311a37e60322f863d1f0c9fa37a2805af07
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-08-31 10:01:37 -0700

    refactor clean script

--- 02_clean.R
+++ 02_clean.R
@@ -10,14 +10,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and limitations under the License.
 
+
 library(tidyr) #wide df to long df
 library(dplyr) #data munging
 library(stringr)
 
+
 ## Read in raw data from 01_load.R if not already in environment
 if (!exists("bc_ghg")) load("tmp/raw_data.RData")
 
-## Convert wide to long format 
+
+## Convert ghg data from wide to long format 
 bc_ghg_long <- bc_ghg %>% 
   gather(key =  year, value = ktCO2e,
        -sector, -subsector_level1,
@@ -32,60 +35,32 @@ bc_ghg_energy <- bc_ghg_long %>%
   mutate(general_source = case_when(subsector_level2 == "Road Transportation" ~ "Road",
                                     subsector_level2 == "Railways" ~ "Railway",
                                     subsector_level3 == "Pipeline Transport" ~ "Pipeline Transport",
-                                    subsector_level3 == "Off-Road Gasoline" ~ "Off-Road",
-                                    subsector_level3 == "Off-Road Diesel" ~ "Off-Road",
+                                    subsector_level2 == "Other Transportation" ~ "Off-Road",
                                     TRUE ~ subsector_level2),
          general_source = str_replace(general_source, "and", "&")) %>% 
+  filter(subsector_level1 != "CO2 Transport and Storage") %>% # all values are all `-` == no emissions
   select(sector, subsector_level1, general_source, year, ktCO2e)
 
 
-## Make 'normalized' columns in bc_pop_gdp gdpdata file for comparison of measures
-##adding ghg pop and gdp normalized columns
-gdpdata$ghgnorm <- gdpdata$BC_GHG_Emissions_ktCO2e/gdpdata$BC_GHG_Emissions_ktCO2e[gdpdata$Year == min(gdpdata$Year)]
-gdpdata$gdpnorm <- gdpdata$GDP/gdpdata$GDP[gdpdata$Year == min(gdpdata$Year)]
-gdpdata$popnorm <- gdpdata$Population/gdpdata$Population[gdpdata$Year == min(gdpdata$Year)]
-
-gdpdata.norm <- subset(gdpdata, select=c("Year", "ghgnorm", "gdpnorm", "popnorm"))
-
-
-##Creating separate ENERGY sector dataframe
-##subsetting rows
-ghgenergy <- subset(ghgdata, ghgdata$sector == "ENERGY")
+## Add ghg totals by year to bc_pop_gdp
+bc_measures <- bc_ghg_long %>%
+  filter(sector != "OTHER LAND USE (Not included in total B.C. emissions)") %>%
+  group_by(year) %>%
+  summarise(ghg_estimate = round(sum(ktCO2e, na.rm=TRUE), digits = 0)) %>% 
+  mutate(year = as.character(year)) %>% 
+  left_join(bc_pop_gdp)
 
 
-##creating 16 generalized subsubsector labels for facet plot
-ghgenergy$general_source <- ghgenergy$subsector_level2
-ghgenergy$general_source[ghgenergy$general_source == "Road Transportation"] <- "Road"
-ghgenergy$general_source[ghgenergy$general_source == "Railways"] <- "Railway"
-ghgenergy$general_source[ghgenergy$subsector_level3 == "Pipeline Transport"] <- "Pipeline Transport"
-ghgenergy$general_source[ghgenergy$subsector_level3 == "Off-Road Gasoline"] <- "Off-Road"
-ghgenergy$general_source[ghgenergy$subsector_level3 == "Off-Road Diesel"] <- "Off-Road"
-
-ghgenergy <- subset(ghgenergy, select=c("sector", "subsector_level1", "general_source",
-                                        "1990","1991","1992","1993","1994","1995",
-                                        "1996","1997","1998","1999","2000","2001",
-                                        "2002","2003","2004","2005","2006","2007",
-                                        "2008","2009","2010","2011", "2012", "2013", "2014"))
-
-##Editing some source names for plot label formatting
-ghgenergy$general_source <- gsub("and", "&", ghgenergy$general_source)
+## Make normalized dateframe for relative comparisons and convert to long format
+normalized_measures <- bc_measures %>% 
+  mutate(norm_ghg = ghg_estimate/ghg_estimate[year == min(year)],
+         norm_gdp = gdp_estimate/gdp_estimate[year == min(year)],
+         norm_population = population_estimate/population_estimate[year == min(year)]) %>% 
+  select(year, starts_with("norm")) %>% 
+  gather(key = measure, value = estimate, -year)
 
 
-##Converting wide files to long format 
-##using reshape to melt the data 
-
-
-ghgenergymelt <- reshape2::melt(ghgenergy, id.vars=c("sector","subsector_level1","general_source"),
-                                variable.name="Year", value.name="ktCO2e")
-
-gdpnormmelt <- reshape2::melt(gdpdata.norm, id.vars=c("Year"),
-                              variable.name="Indicator",
-                              value.name="Amount")
-
-
-##Changing year from factor to character manually
-ghgmelt$year <- as.integer(as.character(ghgmelt$year))
-ghgenergymelt$Year <- as.integer(as.character(ghgenergymelt$Year))
-gdpnormmelt$Year <- as.integer(as.character(gdpnormmelt$Year))
-
+# Create tmp folder if not already there and store objects in local repository
+if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
+save(bc_ghg_long, bc_ghg_energy, bc_measures, normalized_measures, file = "tmp/clean_data.RData")
 
