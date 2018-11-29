20e2947a32b0a153e72fe222fa8c9a0d3e816397
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2018-11-29 14:36:33 -0800

    use mt in place of kt for plots

--- 02_clean.R
+++ 02_clean.R
@@ -33,27 +33,27 @@ bc_ghg_long <- bc_ghg %>%
          year = as.integer(as.character(year)))
 
 
-## Summarize ghg emissions per sector per year
+## Summarize ghg emissions per sector per year and 
+## convert to MtCO2e for (from ktCO2e) for plotting
 ghg_sector_sum <- bc_ghg_long %>%
   filter(sector != "OTHER LAND USE (Not included in total B.C. emissions)") %>%
   group_by(sector, year) %>%
-  summarise(sum = sum(ktCO2e, na.rm = TRUE) %>%
-              round(digits = 0)) %>% 
+  summarise(sum = round(sum(ktCO2e, na.rm = TRUE)/1000, digits = 1)) %>% 
   ungroup() %>% 
   mutate(sector = str_replace(to_titlecase(sector), "and", "&"),
          sector = fct_reorder(sector, sum))
   
 
-## Calculate ghg annual totals
+## Calculate ghg annual totals and convert to MtCO2e (from ktCO2e) for plotting
 bc_ghg_sum <- bc_ghg_long %>%
   filter(sector != "OTHER LAND USE (Not included in total B.C. emissions)") %>%
   group_by(year) %>%
-  summarise(ghg_estimate = round(sum(ktCO2e, na.rm = TRUE), digits = 0)) %>% 
+  summarise(ghg_estimate = round(sum(ktCO2e, na.rm = TRUE)/1000, digits = 1)) %>% 
   mutate(sector = "British Columbia") %>% 
   select(sector, year, ghg_estimate)
 
 
-## Add ghg totals by year to bc_pop_gdp data
+## Add ghg totals in MtCO2e by year to bc_pop_gdp data
 bc_measures <- bc_pop_gdp %>% 
   mutate(year = as.integer(year)) %>%
   left_join(bc_ghg_sum) %>% 
@@ -69,10 +69,11 @@ normalized_measures <- bc_measures %>%
   gather(key = measure, value = estimate, -year)
 
 
-## Calculate GHG emissions per capita & per unit GDP and convert to tCO2e (from ktCO2e)
+## Calculate GHG emissions per capita & per unit GDP 
+## and convert to tCO2e (from MtCO2e) for plotting
 bc_ghg_per_capita <- bc_measures %>% 
-  mutate(ghg_per_capita = (ghg_estimate/population_estimate)*1000,
-         ghg_per_unit_gdp = (ghg_estimate/gdp_estimate)*1000) %>% 
+  mutate(ghg_per_capita = (ghg_estimate/population_estimate)*1000000,
+         ghg_per_unit_gdp = (ghg_estimate/gdp_estimate)*1000000) %>% 
   select(year, ghg_per_capita, ghg_per_unit_gdp)
 
 
@@ -87,11 +88,11 @@ bc_ghg_energy <- bc_ghg_long %>%
          general_source = str_replace(general_source, "and", "&")) %>% 
   select(sector, subsector_level1, general_source, year, ktCO2e)
 
-## Summarize ghg emissions in Energy sector by subsector and general sources
+## Summarize ghg emissions in Energy sector by subsector and general sources 
+## and convert to MtCO2e (from ktCO2e) for plotting
 ghg_energy_group <- bc_ghg_energy %>%
   group_by(sector, subsector_level1, general_source, year) %>%
-  summarise(sum = sum(ktCO2e, na.rm = TRUE) %>%
-              round(digits = 0)) %>%
+  summarise(sum = round(sum(ktCO2e, na.rm = TRUE)/1000, digits = 1)) %>%
   filter(subsector_level1 != "CO2 Transport and Storage") %>% 
   ungroup() %>% 
   mutate(general_source = fct_reorder(general_source, -sum))
@@ -99,12 +100,26 @@ ghg_energy_group <- bc_ghg_energy %>%
 
 ## Data summaries 
 
-## 2016 total
-ghg_est <- bc_ghg_sum %>% 
+## 2016 total in ktCO2e
+ghg_est_ktco2e <- bc_ghg_long %>%
+  filter(sector != "OTHER LAND USE (Not included in total B.C. emissions)") %>%
+  group_by(year) %>%
+  summarise(ghg_estimate = round(sum(ktCO2e, na.rm = TRUE), digits = 0)) %>% 
+  mutate(sector = "British Columbia") %>% 
+  select(sector, year, ghg_estimate) %>% 
   filter(year == "2016") %>% 
-  pull() %>% 
-  signif(digits = 3) %>% 
-  comma()
+  pull()
+ghg_est_ktco2e
+
+ghg_est_Mtco2e <- round(ghg_est_ktco2e/1000, digits = 1)
+
+## Calculate ghg annual totals in ktCO2e
+bc_ghg_sum_ktco2e <- bc_ghg_long %>%
+  filter(sector != "OTHER LAND USE (Not included in total B.C. emissions)") %>%
+  group_by(year) %>%
+  summarise(ghg_estimate = round(sum(ktCO2e, na.rm = TRUE), digits = 0)) %>% 
+  mutate(sector = "British Columbia") %>% 
+  select(sector, year, ghg_estimate)
 
 
 ## GHG emission estimate comparison among years
@@ -120,18 +135,19 @@ calc_inc <- function(ghg_vector, year_vector, since) {
   round(perc, digits = 1)
 }
 
-previous_year <- calc_inc(bc_ghg_sum$ghg_estimate, bc_ghg_sum$year, since = 2015)
+## Comparisons
+previous_year <- calc_inc(bc_ghg_sum_ktco2e$ghg_estimate, bc_ghg_sum_ktco2e$year, since = 2015)
 previous_year
-baseline_year <- calc_inc(bc_ghg_sum$ghg_estimate, bc_ghg_sum$year, since = 2007)
+baseline_year <- calc_inc(bc_ghg_sum_ktco2e$ghg_estimate, bc_ghg_sum_ktco2e$year, since = 2007)
 baseline_year
-three_year <- calc_inc(bc_ghg_sum$ghg_estimate, bc_ghg_sum$year, since = 2013)
+three_year <- calc_inc(bc_ghg_sum_ktco2e$ghg_estimate, bc_ghg_sum_ktco2e$year, since = 2013)
 three_year
-ten_year <- calc_inc(bc_ghg_sum$ghg_estimate, bc_ghg_sum$year, since = 2006)
+ten_year <- calc_inc(bc_ghg_sum_ktco2e$ghg_estimate, bc_ghg_sum_ktco2e$year, since = 2006)
 ten_year
 
 # Create tmp folder if not already there and store clean data in local repository
 if (!exists("tmp")) dir.create("tmp", showWarnings = FALSE)
 save(bc_ghg_long, ghg_sector_sum, bc_ghg_sum, normalized_measures,
      bc_ghg_per_capita, bc_ghg_energy,ghg_energy_group,
-     ghg_est, previous_year, baseline_year, file = "tmp/clean_data.RData")
+     ghg_est_Mtco2e, previous_year, baseline_year, file = "tmp/clean_data.RData")
 
